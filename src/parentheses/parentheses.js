b7a152d179ef71836207df05679148591478cd18
Kyle <kylemcguire35@gmail.com>
2023-12-03 17:48:11 -0800

    completed parenthesis

--- src/parentheses/parentheses.js
+++ src/parentheses/parentheses.js
@@ -1,5 +1,20 @@
 const Stack = require("../lib/stack");
 
-const match = (expression) => {};
+const match = (expression) => {
+  const stack = new Stack();
+
+  for (let i = 0; i < expression.length; i++) {
+    if (expression[i] === "(") {
+      stack.push("(");
+    } else if (expression[i] === ")") {
+      if (stack.top) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return !stack.top;
+};
 
 module.exports = match;
