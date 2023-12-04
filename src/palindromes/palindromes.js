e7dc276f18f4284ecd3b9f26ede56dc381c69ba9
Kyle <kylemcguire35@gmail.com>
2023-12-03 17:30:29 -0800

    completed palindromes

--- src/palindromes/palindromes.js
+++ src/palindromes/palindromes.js
@@ -2,7 +2,24 @@ const Stack = require("../lib/stack");
 
 const isPalindrome = (sentence) => {
   sentence = sentence.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
-  // Your solution here
+  const middle = Math.floor(sentence.length / 2);
+
+  const newStack = new Stack();
+
+  for (let i = 0; i <= middle; i++) {
+    newStack.push(sentence[i]);
+  }
+
+  for (
+    sentence.length % 2 === 0 ? (i = middle) : (i = middle + 1);
+    i > 0;
+    i--
+  ) {
+    const popped = newStack.pop();
+    if (popped !== sentence[i - 1]) return false;
+  }
+
+  return true;
 };
 
 module.exports = isPalindrome;
